2d280d1d2db3df1ba6b3c14dbed4bdd0d76d8c0a
Rian Sunthbocus <rian@rians-mbp.speedport.ip>
2022-09-27 00:36:00 +0200

    Improves the search input field

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,9 +2,14 @@ const searchQuery = document.getElementById('searchQuery');
 const searchBtn = document.getElementById('searchBtn');
 
 let isOpened = false
-searchBtn.style.width = "0"
 
 searchBtn.addEventListener('click', () => {
-    !isOpened;
-    isOpened ? searchBtn.style.width = "100%" : searchBtn.style.width = "0";
+    isOpened = !isOpened;
+    if (isOpened) {
+        searchQuery.style.opacity = "1"
+        searchQuery.style.width = "100%"
+    } else {
+        searchQuery.style.opacity = "0";
+        searchQuery.style.width = "0%";
+    }
 });
